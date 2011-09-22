edf610cd899299a777510154e99bd3e0560abb2f
Ed Andersen <mail@edandersen.com>
2011-09-22 16:26:20 +0900

    Corrected Date initializer

--- src/iscroll.js
+++ src/iscroll.js
@@ -362,7 +362,7 @@ iScroll.prototype = {
 		that.pointX = point.pageX;
 		that.pointY = point.pageY;
 
-		that.startTime = e.timeStamp || Date.now().getTime();
+		that.startTime = e.timeStamp || new Date().getTime();
 
 		if (that.options.onScrollStart) that.options.onScrollStart.call(that, e);
 
@@ -379,7 +379,7 @@ iScroll.prototype = {
 			newX = that.x + deltaX,
 			newY = that.y + deltaY,
 			c1, c2, scale,
-			timestamp = e.timeStamp || Date.now().getTime();
+			timestamp = e.timeStamp || new Date().getTime();
 
 		if (that.options.onBeforeScrollMove) that.options.onBeforeScrollMove.call(that, e);
 
@@ -460,7 +460,7 @@ iScroll.prototype = {
 			target, ev,
 			momentumX = { dist:0, time:0 },
 			momentumY = { dist:0, time:0 },
-			duration = (e.timeStamp || Date.now().getTime()) - that.startTime,
+			duration = (e.timeStamp || new Date().getTime()) - that.startTime,
 			newPosX = that.x,
 			newPosY = that.y,
 			distX, distY,
@@ -690,7 +690,7 @@ iScroll.prototype = {
 	_startAni: function () {
 		var that = this,
 			startX = that.x, startY = that.y,
-			startTime = Date.now().getTime(),
+			startTime = new Date().getTime(),
 			step, easeOut,
 			animate;
 
@@ -718,7 +718,7 @@ iScroll.prototype = {
 		}
 
 		animate = function () {
-			var now = Date.now().getTime(),
+			var now = new Date().getTime(),
 				newX, newY;
 
 			if (now >= startTime + step.time) {
